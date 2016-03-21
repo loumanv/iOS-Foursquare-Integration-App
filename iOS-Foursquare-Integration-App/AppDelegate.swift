5fe30c15dcc167f749326f9cb9b4f9f536f97ae5
Vasileios Loumanis <Billybatigol@billys-mac-pro.home>
2016-03-21 20:44:21 +0000

    Added Session setup with Foursquare app credentials in AppDelegate

--- iOS-Foursquare-Integration-App/AppDelegate.swift
+++ iOS-Foursquare-Integration-App/AppDelegate.swift
@@ -7,40 +7,22 @@
 //
 
 import UIKit
+import QuadratTouch
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let client = Client(clientID: "5ML3MWYZS1FHVCP0YEAJKXFABXJEEPKJHKBMYOAK0VEE1FTR",
+            clientSecret: "H4BLAFYKN5Z2XZHEMYTHJYMW1R5ZIPW1FAZQO2KDUT2BH25N",
+            redirectURL: "ios-app://redirect")
+        var configuration = Configuration(client:client)
+        configuration.mode = "foursquare"
+        Session.setupSharedSessionWithConfiguration(configuration)
         return true
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
