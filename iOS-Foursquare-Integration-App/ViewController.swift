04139c3798422164963d9ba8c910555aeded7844
Vasileios Loumanis <Billybatigol@billys-mac-pro.home>
2016-03-21 20:08:16 +0000

    Initial Commit

---
+++ iOS-Foursquare-Integration-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Foursquare-Integration-App
+//
+//  Created by Vasileios Loumanis on 21/03/2016.
+//  Copyright © 2016 Vasileios Loumanis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
